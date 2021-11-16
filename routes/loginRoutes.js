c795c7c669c5e97a013cc31385ab2326d3be2171
Daniel-Sogbey <mathematics06physics@gmail.com>
2021-11-16 14:29:23 +0000

    added login functionality

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const User = require("../Schemas/userSchema");
 
 const router = express.Router();
 const app = express();
@@ -15,6 +17,34 @@ router.get("/", (req, res, next) => {
 	};
 
 	res.status(200).render("login", payload);
+});
+
+router.post("/", async (req, res, next) => {
+	let payload = req.body;
+
+	if (req.body.username && req.body.password) {
+		const user = await User.findOne({
+			$or: [{ username: req.body.username }, { email: req.body.username }]
+		});
+
+		if (!user) {
+			payload.errorMessage = "No user with the username or password found";
+			res.status(200).render("login", payload);
+		} else {
+			let isMatch = await bcrypt.compare(req.body.password, user.password);
+
+			if (isMatch) {
+				req.session.user = user;
+				return res.redirect("/");
+			} else {
+				payload.errorMessage = "No user with the username or password found";
+				res.status(200).render("login", payload);
+			}
+		}
+	} else {
+		payload.errorMessage = "Make sure each field has a valid value";
+		res.status(200).render("login", payload);
+	}
 });
 
 module.exports = router;
